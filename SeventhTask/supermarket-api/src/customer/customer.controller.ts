54aee28d803df8a0bba353e129c399b1bbc81684
staskukhar <staskuk2005@gmail.com>
2023-11-25 09:39:57 +0200

    adds customer controller

---
+++ SeventhTask/supermarket-api/src/customer/customer.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, HttpException, HttpStatus, Param, ParseIntPipe } from '@nestjs/common';
+import { CustomerService } from './customer.service';
+import { NotFoundError } from 'rxjs';
+
+@Controller('customer')
+export class CustomerController {
+    constructor(
+        private readonly customerService: CustomerService
+    ) {}
+
+    @Get(':id/orders')
+    async getOrdersById(@Param('id', ParseIntPipe) id: number) {
+        const orders = await this.customerService.getOrdersByCustomerId(id);
+        
+        if(orders == null || orders.length === 0) {
+            throw new HttpException('Customer with such id not found', HttpStatus.FORBIDDEN);
+        }
+        return orders;
+    }
+}
